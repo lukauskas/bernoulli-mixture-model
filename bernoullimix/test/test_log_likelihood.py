d2bb36e1fd105c730e7e833083a0ba1404028b44
Saulius Lukauskas <luksaulius@gmail.com>
2015-12-07 23:13:45 +0000

    added the tests for bic and aic with missing data

--- bernoullimix/test/test_log_likelihood.py
+++ bernoullimix/test/test_log_likelihood.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.testing import assert_array_almost_equal
 
 from bernoullimix import BernoulliMixture
-
+import pandas as pd
 
 class TestLogLikelihood(unittest.TestCase):
 
@@ -33,15 +33,7 @@ class TestLogLikelihood(unittest.TestCase):
         self.assertRaises(ValueError, mixture.log_likelihood, dataset_too_few_dims)
         self.assertRaises(ValueError, mixture.log_likelihood, dataset_too_many_dims)
 
-
-    def test_probability_of_generating_a_point_computation(self):
-        """
-        Given a set of parameters for the model.
-        The _point_emission_probs should return a K-sized vector X for each point where:
-        $$
-            x_k = p_k \prod_{d=1}^D \Theta_KD^{x_nd} (1 - \theta_Kd)^(1-x_nd)
-        $$
-        """
+    def test_computation_of_joint(self):
 
         number_of_components = 3
         number_of_dimensions = 4
@@ -73,7 +65,87 @@ class TestLogLikelihood(unittest.TestCase):
 
         assert_array_almost_equal(expected_answer, actual_answer)
 
+    def test_computation_of_joint_with_missing_data(self):
+        number_of_components = 3
+        number_of_dimensions = 4
+
+        sample_mixing_coefficients = np.array([0.5, 0.4, 0.1])
+        sample_eps = np.array([[0.1, 0.2, 0.3, 0.4],
+                                                  [0.1, 0.4, 0.1, 0.4],
+                                                  [0.95, 0.05, 0.05, 0.05]])
+
+        mixture = BernoulliMixture(number_of_components, number_of_dimensions,
+                                   sample_mixing_coefficients, sample_eps)
+
+        sample_dataset = pd.DataFrame(np.array([[None, True, False, False],
+                                                [False, None, False, False],
+                                                [True, True, None, False],
+                                                [False, True, False, None],
+                                                [False, False, False, False],
+                                                [None, None, None, None]]))
+
+        expected_answer = np.empty((len(sample_dataset), number_of_components))
+
+        for sample in range(len(sample_dataset)):
+            for k in range(number_of_components):
+                expected_answer[sample, k] = sample_mixing_coefficients[k]
+
+                row = sample_dataset.iloc[sample]
+
+                for d in range(len(row)):
+                    if row[d] is None:
+                        continue
+                    expected_answer[sample, k] *= np.power(sample_eps[k, d], row[d])
+                    expected_answer[sample, k] *= np.power(1-sample_eps[k, d], 1-row[d])
+
+        actual_answer = mixture._prob_z_o_given_theta(sample_dataset)
+
+        assert_array_almost_equal(expected_answer, actual_answer)
+
     def test_log_likelihood_produces_correct_answer(self):
+        """
+        Given a set of parameters, and an appropriate dataset, log likelihood should
+        return:
+
+        $$
+           \sum_n log \sum_k p_k \prod_{d=1}^D \Theta_KD^{x_nd} (1 - \theta_Kd)^(1-x_nd)
+        $$
+        """
+
+        number_of_components = 3
+        number_of_dimensions = 4
+
+        sample_mixing_coefficients = np.array([0.5, 0.4, 0.1])
+        sample_emission_probabilities = np.array([[0.1, 0.2, 0.3, 0.4],
+                                                  [0.1, 0.4, 0.1, 0.4],
+                                                  [0.95, 0.05, 0.05, 0.05]])
+
+        mixture = BernoulliMixture(number_of_components, number_of_dimensions,
+                                   sample_mixing_coefficients, sample_emission_probabilities)
+
+        sample_dataset = pd.DataFrame(np.array([[None, True, False, False],
+                                                [False, None, False, False],
+                                                [True, True, None, False],
+                                                [False, True, False, None],
+                                                [False, False, False, False],
+                                                [True, True, False, False]]))
+
+        unique_dataset, weights = BernoulliMixture.aggregate_dataset(sample_dataset)
+
+        # Compute support on whole dataset for the test
+        # even though code would compute it for unique_dataset only
+        support = mixture._prob_z_o_given_theta(sample_dataset)
+        expected_answer = np.sum(np.log(np.sum(support, axis=1)))
+
+        actual_answer = mixture.log_likelihood(sample_dataset)
+
+        unique_support = mixture._prob_z_o_given_theta(unique_dataset)
+        actual_answer_from_support = mixture._log_likelihood_from_support(unique_support, weights)
+
+        self.assertAlmostEqual(expected_answer, actual_answer)
+        self.assertAlmostEqual(expected_answer, actual_answer_from_support)
+
+    def test_log_likelihood_produces_correct_answer_with_missing_data(self):
         """
         Given a set of parameters, and an appropriate dataset, log likelihood should
         return:
@@ -99,7 +171,7 @@ class TestLogLikelihood(unittest.TestCase):
                                    [True, True, False, False],
                                    [False, True, False, False],
                                    [False, False, False, False],
-                                   [True, True, False, False]])
+                                   [None, None, None, None]])
 
         unique_dataset, weights = BernoulliMixture.aggregate_dataset(sample_dataset)
 
@@ -115,3 +187,136 @@ class TestLogLikelihood(unittest.TestCase):
 
         self.assertAlmostEqual(expected_answer, actual_answer)
         self.assertAlmostEqual(expected_answer, actual_answer_from_support)
+
+
+class TestPenalisedLikelihood(unittest.TestCase):
+
+    def test_number_of_free_parameters_computed_correctly(self):
+        number_of_components = 3
+        number_of_dimensions = 4
+
+        sample_mixing_coefficients = np.array([0.5, 0.4, 0.1])
+        sample_emission_probabilities = np.array([[0.1, 0.2, 0.3, 0.4],
+                                                  [0.1, 0.4, 0.1, 0.4],
+                                                  [0.5, 0.5, 0.5, 0.5]])
+
+        mixture = BernoulliMixture(number_of_components, number_of_dimensions,
+                                   sample_mixing_coefficients, sample_emission_probabilities)
+
+        expected_number_of_free_parameters = (number_of_components - 1) + \
+                                             (number_of_dimensions * number_of_components)
+        actual_number_of_free_parameters = mixture.number_of_free_parameters
+
+        self.assertEqual(actual_number_of_free_parameters, expected_number_of_free_parameters)
+
+    def test_bic_computed_correctly(self):
+        number_of_components = 3
+
+        number_of_dimensions = 4
+
+        sample_mixing_coefficients = np.array([0.5, 0.4, 0.1])
+        sample_emission_probabilities = np.array([[0.1, 0.2, 0.3, 0.4],
+                                                  [0.1, 0.4, 0.1, 0.4],
+                                                  [0.5, 0.5, 0.5, 0.5]])
+
+        mixture = BernoulliMixture(number_of_components, number_of_dimensions,
+                                   sample_mixing_coefficients, sample_emission_probabilities)
+
+        dataset = np.array([[True, True, False, False],
+                            [False, True, False, False],
+                            [True, True, False, False],
+                            [False, True, False, False],
+                            [False, False, False, False],
+                            [True, True, False, False]])
+
+        log_likelihood = mixture.log_likelihood(dataset)
+        number_of_free_parameters = mixture.number_of_free_parameters
+
+        expected_bic = -2 * log_likelihood + number_of_free_parameters * np.log(len(dataset))
+        actual_bic = mixture.BIC_dataset(dataset)
+
+        self.assertEqual(expected_bic, actual_bic)
+
+    def test_bic_computed_correctly_with_missing_data(self):
+        number_of_components = 3
+
+        number_of_dimensions = 4
+
+        sample_mixing_coefficients = np.array([0.5, 0.4, 0.1])
+        sample_emission_probabilities = np.array([[0.1, 0.2, 0.3, 0.4],
+                                                  [0.1, 0.4, 0.1, 0.4],
+                                                  [0.5, 0.5, 0.5, 0.5]])
+
+        mixture = BernoulliMixture(number_of_components, number_of_dimensions,
+                                   sample_mixing_coefficients, sample_emission_probabilities)
+
+        dataset = pd.DataFrame(np.array([[None, True, False, False],
+                                         [False, None, False, False],
+                                         [True, True, None, False],
+                                         [False, True, False, None],
+                                         [False, False, True, False],
+                                         [None, None, None, None]]))
+
+        log_likelihood = mixture.log_likelihood(dataset)
+        number_of_free_parameters = mixture.number_of_free_parameters
+
+        expected_bic = -2 * log_likelihood + number_of_free_parameters * np.log(len(dataset))
+        actual_bic = mixture.BIC_dataset(dataset)
+
+        self.assertEqual(expected_bic, actual_bic)
+
+    def test_aic_computed_correctly(self):
+        number_of_components = 3
+
+        number_of_dimensions = 4
+
+        sample_mixing_coefficients = np.array([0.5, 0.4, 0.1])
+        sample_emission_probabilities = np.array([[0.1, 0.2, 0.3, 0.4],
+                                                  [0.1, 0.4, 0.1, 0.4],
+                                                  [0.5, 0.5, 0.5, 0.5]])
+
+        mixture = BernoulliMixture(number_of_components, number_of_dimensions,
+                                   sample_mixing_coefficients, sample_emission_probabilities)
+
+        dataset = np.array([[True, True, False, False],
+                            [False, True, False, False],
+                            [True, True, False, False],
+                            [False, True, False, False],
+                            [False, False, False, False],
+                            [True, True, False, False]])
+
+        log_likelihood = mixture.log_likelihood(dataset)
+        number_of_free_parameters = mixture.number_of_free_parameters
+
+        expected_aic = -2 * log_likelihood + number_of_free_parameters * 2
+        actual_aic = mixture.AIC_dataset(dataset)
+
+        self.assertEqual(expected_aic, actual_aic)
+
+    def test_aic_computed_correctly_with_missing_data(self):
+        number_of_components = 3
+
+        number_of_dimensions = 4
+
+        sample_mixing_coefficients = np.array([0.5, 0.4, 0.1])
+        sample_emission_probabilities = np.array([[0.1, 0.2, 0.3, 0.4],
+                                                  [0.1, 0.4, 0.1, 0.4],
+                                                  [0.5, 0.5, 0.5, 0.5]])
+
+        mixture = BernoulliMixture(number_of_components, number_of_dimensions,
+                                   sample_mixing_coefficients, sample_emission_probabilities)
+
+        dataset = pd.DataFrame(np.array([[None, True, False, False],
+                                         [False, None, False, False],
+                                         [True, True, None, False],
+                                         [False, True, False, None],
+                                         [False, False, True, False],
+                                         [None, None, None, None]]))
+
+        log_likelihood = mixture.log_likelihood(dataset)
+        number_of_free_parameters = mixture.number_of_free_parameters
+
+        expected_aic = -2 * log_likelihood + number_of_free_parameters * 2
+        actual_aic = mixture.AIC_dataset(dataset)
+
+        self.assertEqual(expected_aic, actual_aic)
