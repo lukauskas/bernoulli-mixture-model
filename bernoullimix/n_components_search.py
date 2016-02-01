c63499838d09bbc252df4f4f4b65efe648e14a73
Saulius Lukauskas <luksaulius@gmail.com>
2016-02-01 09:37:25 +0000

    committing new changes

--- bernoullimix/n_components_search.py
+++ bernoullimix/n_components_search.py
@@ -3,48 +3,70 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 import itertools
+import multiprocessing
+
 from bernoullimix import MultiDatasetMixtureModel
 from bernoullimix.mixture import WEIGHT_COLUMN
 from bernoullimix.random_initialisation import random_mixture_generator
 import pandas as pd
 
+def _initializer(data, random_state, n_mixtures_to_search, fit_kwargs):
+    global g_data, g_random_state, g_n_mixtures_to_search, g_fit_kwargs
 
-def search_k(k_range_to_search, data, components_per_k=10, random_state=None, **fit_kwargs):
+    n_mixtures_to_search = int(n_mixtures_to_search)
+    g_n_mixtures_to_search = n_mixtures_to_search
+
+    g_data = data
+    g_random_state = random_state
+    g_fit_kwargs = fit_kwargs
+
+
+def _map_function(k):
+    global g_data, g_random_state, g_n_mixtures_to_search, g_fit_kwargs
+
+    generator = random_mixture_generator(k, g_data,
+                                         random_state=g_random_state)
+
+    mixtures = list(itertools.islice(generator, g_n_mixtures_to_search))
+
+    best_result = None
+    best_mixture = None
+
+    for mixture in mixtures:
+        result = mixture.fit(g_data, **g_fit_kwargs)
+        result = pd.Series(result, index=['converged', 'n_iterations', 'log_likelihood'])
+        if best_result is None or result['log_likelihood'] > best_result['log_likelihood']:
+            best_result = result
+            best_mixture = mixture
+
+    best_result['BIC'] = best_mixture.BIC(best_result['log_likelihood'],
+                                          g_data[WEIGHT_COLUMN].sum())
+
+    return best_result, best_mixture
+
+
+def search_k(k_range_to_search, data, mixtures_per_k=10,
+             random_state=None,
+             n_jobs=1,
+             **fit_kwargs):
 
     data = MultiDatasetMixtureModel.collapse_dataset(data)
-    sum_of_weights = data[WEIGHT_COLUMN].sum()
 
-    best_mixtures = {}
-    best_results = {}
+    pool = multiprocessing.Pool(processes=n_jobs,
+                                initializer=_initializer,
+                                initargs=(data, random_state, mixtures_per_k, fit_kwargs)
+                                )
 
-    for k in k_range_to_search:
-        mixtures_k = list(itertools.islice(random_mixture_generator(k, data, random_state=random_state),
-                                           components_per_k))
+    results = pool.map(_map_function, k_range_to_search)
 
-        results = map(lambda x: x.fit(data, **fit_kwargs), mixtures_k)
+    results_df = {}
+    results_mixtures = {}
 
-        best_result = None
-        best_mixture = None
+    for k, (result, mixture) in zip(k_range_to_search, results):
+        results_df[k] = result
+        results_mixtures[k] = mixture
 
-        for result, mixture in zip(results, mixtures_k):
-            result = pd.Series(result, index=['converged', 'n_iterations', 'log_likelihood'])
-            if best_result is None or result['log_likelihood'] > best_result['log_likelihood']:
-                best_result = result
-                best_mixture = mixture
+    results_df = pd.DataFrame(results_df).T
+    results_df.columns.name = 'n_components'
 
-        best_mixtures[k] = best_mixture
-        best_results[k] = best_result
-
-    best_results = pd.DataFrame(best_results).T
-
-    bics = {}
-    for k in k_range_to_search:
-        mixture = best_mixtures[k]
-        bic = mixture.BIC(best_results.loc[k, 'log_likelihood'], sum_of_weights)
-
-        bics[k] = bic
-
-    bics = pd.Series(bics)
-    best_results['BIC'] = bics
-
-    return best_mixtures, best_results
+    return results_df, results_mixtures
